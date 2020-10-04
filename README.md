e96f19747f6c8162ecbc06619c94e9aae2500f76
Thibault Hallouin <thibault.hallouin@ucdconnect.ie>
2020-10-04 18:04:42 +0200

    add RST docstrings to evaluator and all objective functions
    also improve compliance with PEP8 (on the max number of characters per line)

--- README.md
+++ README.md
@@ -4,57 +4,21 @@
 
 # HydroEval - An efficient evaluator for streamflow time series in Python
 
-HydroEval is an open-source evaluator for streamflow time series in Python. It is licensed under GNU GPL-3.0 (see [licence file](https://github.com/ThibHlln/hydroeval/blob/master/LICENCE.md) provided). The purpose of this evaluator is to compare observed and simulated hydrographs using one or more objective functions. HydroEval is designed to calculate all objective functions in a vectorised manner (using [numpy](https://github.com/numpy/numpy), and therefore C code in the background) which makes for very efficient computation of the objective functions.
+`hydroeval` is an open-source `evaluator` of goodness of fit between simulated 
+and observed streamflow time series in Python. It is licensed under GNU GPL-3.0 
+(see [licence file](https://github.com/ThibHlln/hydroeval/blob/master/LICENCE.md) 
+provided). The package provides a bundle of the most commonly used objective 
+functions in hydrological science. The package is designed to calculate all 
+objective functions in a vectorised manner (using [numpy](https://github.com/numpy/numpy), 
+and therefore C code in the background) which makes for very efficient 
+computation of the objective functions.
 
-## How to Install
+Please refer to the [online documentation](https://thibhlln.github.io/hydroeval) 
+for further details and learn how to use the package. 
 
-HydroEval is available on PyPI, so you can simply use pip and the name of the package:
-
-    python -m pip install hydroeval
-
-You can also use pip and a link to the GitHub repository directly:
-
-	python -m pip install git+https://github.com/ThibHlln/hydroeval.git
-
-Alternatively, you can download the source code (*i.e.* the GitHub repository) and, from the downloaded directory itself, run the command:
-
-    python setup.py install
-
-## How to Use
-
-A tutorial in the form of a [Jupyter notebook](https://github.com/ThibHlln/hydroeval/blob/master/examples/api_usage_example.ipynb) is available to get started with the usage of HydroEval's API. The input files required for the tutorial are all provided in the `examples/` folder.
-
-## How to Cite
-
-If you are using HydroEval, please consider citing the software as follows (click on the link to get the DOI of a specific version):
+If you are using `hydroeval`, please consider citing the software as follows 
+(click on the link to get the DOI of a specific version):
 * Hallouin, T. (XXXX). HydroEval: Streamflow Simulations Evaluator (Version X.X.X). Zenodo. https://doi.org/10.5281/zenodo.2591217
-
-## Objective Functions Available
-
-The objective functions currently available in HydroEval to evaluate the fit between observed and simulated stream flow time series are as follows:
-* [Nash-Sutcliffe Efficiency](https://doi.org/10.1016/0022-1694(70)90255-6) (`nse`)
-* [Original Kling-Gupta Efficiency](https://doi.org/10.1016/j.jhydrol.2009.08.003) (`kge`) and its three components (r, α, β)
-* [Modified Kling-Gupta Efficiency](https://doi.org/10.1016/j.jhydrol.2012.01.011) (`kgeprime`) and its three components (r, γ, β)
-* [Non-Parametric Kling-Gupta Efficiency](https://doi.org/10.1080/02626667.2018.1552002) (`kgenp`) and its three components (r, α, β)
-* Root Mean Square Error (`rmse`)
-* Mean Absolute Relative Error (`mare`)
-* Percent Bias (`pbias`)
-
-Moreover, KGE and NSE can be calculated in a bounded version following [Mathevet et al. (2006)](https://iahs.info/uploads/dms/13614.21--211-219-41-MATHEVET.pdf):
-
-* Bounded Nash-Sutcliffe Efficiency (`nse_c2m`)
-* Bounded Original Kling-Gupta Efficiency (`kge_c2m`)
-* Bounded Modified Kling-Gupta Efficiency (`kgeprime_c2m`)
-* Bounded Non-Parametric Kling-Gupta Efficiency (`kgenp_c2m`)
-
-Finally, any of the objective functions can take an optional argument `transform`. This argument allows to apply a transformation on both the observed and the simulated streamflow time series prior the calculation of the objective function. The possible transformations are as follows:
-* Inverted flows (using `transform='inv'`)
-* Square Root-transformed flows (using `transform='sqrt'`)
-* Natural Logarithm-transformed flows (using `transform='log'`)
-
-## Dependencies
-
-HydroEval requires the Python package `numpy` to be installed on the Python interpreter where `hydroeval` is installed.
 
 ## Version History
 
@@ -64,4 +28,5 @@ HydroEval requires the Python package `numpy` to be installed on the Python inte
 
 ## Acknowledgment
 
-This tool was developed with the financial support of Ireland's Environmental Protection Agency (Grant Number 2014-W-LS-5).
+Early versions of this tool were developed with the financial support of 
+Ireland's Environmental Protection Agency (Grant Number 2014-W-LS-5).
--- hydroeval/objective_functions.py
+++ hydroeval/objective_functions.py
@@ -21,129 +21,305 @@
 import numpy as np
 
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # OBJECTIVE FUNCTIONS
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Nash-Sutcliffe Efficiency (Nash and Sutcliffe 1970 - https://doi.org/10.1016/0022-1694(70)90255-6)
-def nse(simulation_s, evaluation):
-    nse_ = 1 - (np.sum((evaluation - simulation_s) ** 2, axis=0, dtype=np.float64) /
-                np.sum((evaluation - np.mean(evaluation)) ** 2, dtype=np.float64))
+def nse(simulations, evaluation):
+    """Nash-Sutcliffe Efficiency (NSE) as per `Nash and Sutcliffe, 1970
+    <https://doi.org/10.1016/0022-1694(70)90255-6>`_.
+
+    :Calculation Details:
+        .. math::
+           E_{\\text{NSE}} = 1 - \\frac{\\sum_{i=1}^{N}[e_{i}-s_{i}]^2}
+           {\\sum_{i=1}^{N}[e_{i}-\\mu(e)]^2}
+
+        where *N* is the length of the *simulations* and *evaluation*
+        periods, *e* is the *evaluation* series, *s* is (one of) the
+        *simulations* series, and *μ* is the arithmetic mean.
+
+    """
+    nse_ = 1 - (
+            np.sum((evaluation - simulations) ** 2, axis=0, dtype=np.float64)
+            / np.sum((evaluation - np.mean(evaluation)) ** 2, dtype=np.float64)
+    )
 
     return nse_
 
 
-# Original Kling-Gupta Efficiency (Gupta et al. 2009 - https://doi.org/10.1016/j.jhydrol.2009.08.003)
-def kge(simulation_s, evaluation):
-    # calculate error in timing and dynamics r (Pearson's correlation coefficient)
-    sim_mean = np.mean(simulation_s, axis=0, dtype=np.float64)
+def kge(simulations, evaluation):
+    """Original Kling-Gupta Efficiency (KGE) and its three components
+    (r, α, β) as per `Gupta et al., 2009
+    <https://doi.org/10.1016/j.jhydrol.2009.08.003>`_.
+
+    Note, all four values KGE, r, α, β are returned, in this order.
+
+    :Calculation Details:
+        .. math::
+           E_{\\text{KGE}} = 1 - \\sqrt{[r - 1]^2 + [\\alpha - 1]^2
+           + [\\beta - 1]^2}
+        .. math::
+           r = \\frac{\\text{cov}(e, s)}{\\sigma({e}) \\cdot \\sigma(s)}
+        .. math::
+           \\alpha = \\frac{\\sigma(s)}{\\sigma(e)}
+        .. math::
+           \\beta = \\frac{\\mu(s)}{\\mu(e)}
+
+        where *e* is the *evaluation* series, *s* is (one of) the
+        *simulations* series, *cov* is the covariance, *σ* is the
+        standard deviation, and *μ* is the arithmetic mean.
+
+    """
+    # calculate error in timing and dynamics r
+    # (Pearson's correlation coefficient)
+    sim_mean = np.mean(simulations, axis=0, dtype=np.float64)
     obs_mean = np.mean(evaluation, dtype=np.float64)
-    r = np.sum((simulation_s - sim_mean) * (evaluation - obs_mean), axis=0, dtype=np.float64) / \
-        np.sqrt(np.sum((simulation_s - sim_mean) ** 2, axis=0, dtype=np.float64) *
-                np.sum((evaluation - obs_mean) ** 2, dtype=np.float64))
+
+    r_num = np.sum((simulations - sim_mean) * (evaluation - obs_mean),
+                   axis=0, dtype=np.float64)
+    r_den = np.sqrt(np.sum((simulations - sim_mean) ** 2,
+                           axis=0, dtype=np.float64)
+                    * np.sum((evaluation - obs_mean) ** 2,
+                             dtype=np.float64))
+    r = r_num / r_den
     # calculate error in spread of flow alpha
-    alpha = np.std(simulation_s, axis=0) / np.std(evaluation, dtype=np.float64)
+    alpha = np.std(simulations, axis=0) / np.std(evaluation, dtype=np.float64)
     # calculate error in volume beta (bias of mean discharge)
-    beta = np.sum(simulation_s, axis=0, dtype=np.float64) / np.sum(evaluation, dtype=np.float64)
+    beta = (np.sum(simulations, axis=0, dtype=np.float64)
+            / np.sum(evaluation, dtype=np.float64))
     # calculate the Kling-Gupta Efficiency KGE
     kge_ = 1 - np.sqrt((r - 1) ** 2 + (alpha - 1) ** 2 + (beta - 1) ** 2)
 
     return np.vstack((kge_, r, alpha, beta))
 
 
-# Modified Kling-Gupta Efficiency (Kling et al. 2012 - https://doi.org/10.1016/j.jhydrol.2012.01.011)
-def kgeprime(simulation_s, evaluation):
+def kgeprime(simulations, evaluation):
+    """Modified Kling-Gupta Efficiency (KGE') and its three components
+    (r, γ, β) as per `Kling et al., 2012
+    <https://doi.org/10.1016/j.jhydrol.2012.01.011>`_.
+
+    Note, all four values KGE', r, γ, β are returned, in this order.
+
+    :Calculation Details:
+        .. math::
+           E_{\\text{KGE'}} = 1 - \\sqrt{[r - 1]^2 + [\\gamma - 1]^2
+           + [\\beta - 1]^2}
+        .. math::
+           r = \\frac{\\text{cov}(e, s)}{\\sigma({e}) \\cdot \\sigma(s)}
+        .. math::
+           \\gamma = \\frac{\\sigma(s) / \\mu(s)}{\\sigma(e) / \\mu(e)}
+        .. math::
+           \\beta = \\frac{\\mu(s)}{\\mu(e)}
+
+        where *e* is the *evaluation* series, *s* is (one of) the
+        *simulations* series, *cov* is the covariance, *σ* is the
+        standard deviation, and *μ* is the arithmetic mean.
+
+    """
     # calculate error in timing and dynamics r (Pearson's correlation coefficient)
-    sim_mean = np.mean(simulation_s, axis=0, dtype=np.float64)
+    sim_mean = np.mean(simulations, axis=0, dtype=np.float64)
     obs_mean = np.mean(evaluation, dtype=np.float64)
-    r = np.sum((simulation_s - sim_mean) * (evaluation - obs_mean), axis=0, dtype=np.float64) / \
-        np.sqrt(np.sum((simulation_s - sim_mean) ** 2, axis=0, dtype=np.float64) *
+    r = np.sum((simulations - sim_mean) * (evaluation - obs_mean), axis=0, dtype=np.float64) / \
+        np.sqrt(np.sum((simulations - sim_mean) ** 2, axis=0, dtype=np.float64) *
                 np.sum((evaluation - obs_mean) ** 2, dtype=np.float64))
     # calculate error in spread of flow gamma (avoiding cross correlation with bias by dividing by the mean)
-    gamma = (np.std(simulation_s, axis=0, dtype=np.float64) / sim_mean) / \
-        (np.std(evaluation, dtype=np.float64) / obs_mean)
+    gamma = (np.std(simulations, axis=0, dtype=np.float64) / sim_mean) / \
+            (np.std(evaluation, dtype=np.float64) / obs_mean)
     # calculate error in volume beta (bias of mean discharge)
-    beta = np.mean(simulation_s, axis=0, dtype=np.float64) / np.mean(evaluation, axis=0, dtype=np.float64)
+    beta = np.mean(simulations, axis=0, dtype=np.float64) / np.mean(evaluation, axis=0, dtype=np.float64)
     # calculate the modified Kling-Gupta Efficiency KGE'
     kgeprime_ = 1 - np.sqrt((r - 1) ** 2 + (gamma - 1) ** 2 + (beta - 1) ** 2)
 
     return np.vstack((kgeprime_, r, gamma, beta))
 
 
-# Non-Parametric Kling-Gupta Efficiency (Pool et al. 2018 - https://doi.org/10.1080/02626667.2018.1552002)
-def kgenp(simulation_s, evaluation):
+def kgenp(simulations, evaluation):
+    """Non-Parametric Kling-Gupta Efficiency (KGE\\ :sub:`NP`\\) and its
+    three components (r\\ :sub:`S`\\ , α\\ :sub:`NP`\\, β) as per
+    `Pool et al., 2018 <https://doi.org/10.1080/02626667.2018.1552002>`_.
+
+    Note, all four values KGE\\ :sub:`NP`\\, r\\ :sub:`S`\\ , α\\ :sub:`NP`\\,
+    β are returned, in this order.
+
+    :Calculation Details:
+        .. math::
+           E_{\\text{KGE}_\\text{NP}} = 1 - \\sqrt{[r_{\\text{S}} - 1]^2
+           + [\\alpha_{\\text{NP}} - 1]^2 + [\\beta - 1]^2}
+        .. math::
+           r_{\\text{S}} = \\frac{\\sum_{i=1}^{N} [E_i-\\mu(E)] [S_i-\\mu(S)]}
+           {\\sqrt{ \\big[ \\sum_{i=1}^{N}[E_i-\\mu(E)]^2 \\big]
+           \\big[ \\sum_{i=1}^{N} [S_i-\\mu(S)]^2 \\big]}}
+        .. math::
+           \\alpha_{\\text{NP}} = 1 - \\frac{1}{2} \\sum_{k=1}^{N}
+           \\Bigl| \\frac{s_{I(k)}}{N \\cdot \\mu(s)}
+           - \\frac{e_{I(k)}}{N \\cdot \\mu(e)} \\Bigr|
+        .. math::
+           \\beta = \\frac{\\mu(s)}{\\mu(e)}
+
+        where *N* is the length of the *simulations* and *evaluation*
+        periods, *e* is the *evaluation* series, *s* is (one of) the
+        *simulations* series, *E* and *S* are the series of ranks for
+        the *e* and *s* series of streamflow, respectively, *I* and *J*
+        are the functions giving the indices of the *k*\\ :sup:`th` \\
+        largest flood in *s* and *e* series, respectively, and *μ* is
+        the arithmetic mean.
+
+    """
     # calculate error in timing and dynamics r (Spearman's correlation coefficient)
-    sim_rank = np.argsort(np.argsort(simulation_s, axis=0), axis=0)
+    sim_rank = np.argsort(np.argsort(simulations, axis=0), axis=0)
     obs_rank = np.argsort(np.argsort(evaluation, axis=0), axis=0)
     r = np.sum((obs_rank - np.mean(obs_rank, axis=0, dtype=np.float64)) *
                (sim_rank - np.mean(sim_rank, axis=0, dtype=np.float64)), axis=0) / \
         np.sqrt(np.sum((obs_rank - np.mean(obs_rank, axis=0, dtype=np.float64)) ** 2, axis=0) *
                 (np.sum((sim_rank - np.mean(sim_rank, axis=0, dtype=np.float64)) ** 2, axis=0)))
     # calculate error in timing and dynamics alpha (flow duration curve)
-    sim_fdc = np.sort(simulation_s / (simulation_s.shape[0] * np.mean(simulation_s, axis=0, dtype=np.float64)), axis=0)
+    sim_fdc = np.sort(simulations / (simulations.shape[0] * np.mean(simulations, axis=0, dtype=np.float64)), axis=0)
     obs_fdc = np.sort(evaluation / (evaluation.shape[0] * np.mean(evaluation, axis=0, dtype=np.float64)), axis=0)
     alpha = 1 - 0.5 * np.sum(np.abs(sim_fdc - obs_fdc), axis=0)
     # calculate error in volume beta (bias of mean discharge)
-    beta = np.mean(simulation_s, axis=0) / np.mean(evaluation, axis=0, dtype=np.float64)
+    beta = np.mean(simulations, axis=0) / np.mean(evaluation, axis=0, dtype=np.float64)
     # calculate the non-parametric Kling-Gupta Efficiency KGEnp
     kgenp_ = 1 - np.sqrt((r - 1) ** 2 + (alpha - 1) ** 2 + (beta - 1) ** 2)
 
     return np.vstack((kgenp_, r, alpha, beta))
 
 
-# Root Mean Square Error
-def rmse(simulation_s, evaluation):
-    rmse_ = np.sqrt(np.mean((evaluation - simulation_s) ** 2, axis=0, dtype=np.float64))
+def rmse(simulations, evaluation):
+    """Root Mean Square Error (RMSE).
+
+    :Calculation Details:
+        .. math::
+           E_{\\text{RMSE}} = \\sqrt{\\frac{1}{N}\\sum_{i=1}^{N}[e_i-s_i]^2}
+
+        where *N* is the length of the *simulations* and *evaluation*
+        periods, *e* is the *evaluation* series, *s* is (one of) the
+        *simulations* series.
+
+    """
+    rmse_ = np.sqrt(np.mean((evaluation - simulations) ** 2, axis=0, dtype=np.float64))
 
     return rmse_
 
 
-# Mean Absolute Relative Error
-def mare(simulation_s, evaluation):
-    mare_ = np.sum(np.abs(evaluation - simulation_s), axis=0, dtype=np.float64) / np.sum(evaluation)
+def mare(simulations, evaluation):
+    """Mean Absolute Relative Error (MARE).
+
+    :Calculation Details:
+        .. math::
+           E_{\\text{MARE}} = \\frac{\\sum_{i=1}^{N} \\left| e_i-s_i \\right|}
+           {\\sum_{i=1}^{N} e_i}
+
+        where *N* is the length of the *simulations* and *evaluation*
+        periods, *e* is the *evaluation* series, *s* is (one of) the
+        *simulations* series.
+
+    """
+    mare_ = np.sum(np.abs(evaluation - simulations), axis=0, dtype=np.float64) / np.sum(evaluation)
 
     return mare_
 
 
-# Percent Bias
-def pbias(simulation_s, evaluation):
-    pbias_ = 100 * np.sum(evaluation - simulation_s, axis=0, dtype=np.float64) / np.sum(evaluation)
+def pbias(simulations, evaluation):
+    """Percent Bias (PBias).
+
+    :Calculation Details:
+        .. math::
+           E_{PBias} = 100 × \\frac{\\sum_{i=1}^{N}(e_{i}-s_{i})}{\\sum_{i=1}^{N}e_{i}}
+
+        where *N* is the length of the *simulations* and *evaluation*
+        periods, *e* is the *evaluation* series, and *s* is (one of)
+        the *simulations* series.
+
+    """
+    pbias_ = 100 * np.sum(evaluation - simulations, axis=0, dtype=np.float64) / np.sum(evaluation)
 
     return pbias_
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # BOUNDED VERSIONS OF SOME OBJECTIVE FUNCTIONS
-# (After Mathevet et al. 2006 - https://iahs.info/uploads/dms/13614.21--211-219-41-MATHEVET.pdf)
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-# Bounded Version of the Nash-Sutcliffe Efficiency
-def nse_c2m(simulation_s, evaluation):
-    nse_ = nse(simulation_s, evaluation)
+def nse_c2m(simulations, evaluation):
+    """Nash-Sutcliffe Efficiency (NSE) in a bounded version as per
+    `Mathevet et al., 2006
+    <https://iahs.info/uploads/dms/13614.21--211-219-41-MATHEVET.pdf>`_.
+
+    See `nse` for details on NSE.
+
+    :Calculation Details:
+        .. math::
+           E_{\\text{NSE}_{\\text{C2M}}} = \\frac{E_{\\text{NSE}}}
+           {2 - E_{\\text{NSE}}}
+
+    """
+    nse_ = nse(simulations, evaluation)
     nse_c2m_ = nse_ / (2 - nse_)
 
     return nse_c2m_
 
 
-# Bounded Version of the Original Kling-Gupta Efficiency
-def kge_c2m(simulation_s, evaluation):
-    kge_ = kge(simulation_s, evaluation)[0, :]
+def kge_c2m(simulations, evaluation):
+    """Original Kling-Gupta Efficiency (KGE) in a bounded version as per
+    `Mathevet et al., 2006
+    <https://iahs.info/uploads/dms/13614.21--211-219-41-MATHEVET.pdf>`_.
+
+    See `kge` for details on KGE.
+
+    Note, only the composite objective function KGE is bounded and
+    returned, not its constituents r, α, β.
+
+    :Calculation Details:
+        .. math::
+           E_{\\text{KGE,C2M}} = \\frac{E_{\\text{KGE}}}
+           {2 - E_{\\text{KGE}}}
+
+    """
+    kge_ = kge(simulations, evaluation)[0, :]
     kge_c2m_ = kge_ / (2 - kge_)
 
     return kge_c2m_
 
 
-# Bounded Version of the Modified Kling-Gupta Efficiency
-def kgeprime_c2m(simulation_s, evaluation):
-    kgeprime_ = kgeprime(simulation_s, evaluation)[0, :]
+def kgeprime_c2m(simulations, evaluation):
+    """Modified Kling-Gupta Efficiency (KGE') in a bounded version as
+    per `Mathevet et al., 2006
+    <https://iahs.info/uploads/dms/13614.21--211-219-41-MATHEVET.pdf>`_.
+
+    See `kgeprime` for details on KGE'.
+
+    Note, only the composite objective function KGE' is bounded and
+    returned, not its constituents r, γ, β.
+
+    :Calculation Details:
+        .. math::
+           E_{\\text{KGE',C2M}} = \\frac{E_{\\text{KGE'}}}
+           {2 - E_{\\text{KGE'}}}
+
+    """
+    kgeprime_ = kgeprime(simulations, evaluation)[0, :]
     kgeprime_c2m_ = kgeprime_ / (2 - kgeprime_)
 
     return kgeprime_c2m_
 
 
-# Bounded Version of the Non-Parametric Kling-Gupta Efficiency
-def kgenp_c2m(simulation_s, evaluation):
-    kgenp_ = kgenp(simulation_s, evaluation)[0, :]
+def kgenp_c2m(simulations, evaluation):
+    """Non-Parametric Kling-Gupta Efficiency (KGEnp) in a bounded
+    version as per `Mathevet et al., 2006
+    <https://iahs.info/uploads/dms/13614.21--211-219-41-MATHEVET.pdf>`_.
+
+    See `kgenp` for details on KGEnp.
+
+    Note, only the composite objective function KGEnp is bounded and
+    returned, not its constituents r, α, β.
+
+    :Calculation Details:
+        .. math::
+           E_{\\text{KGE}_\\text{NP}\\text{,C2M}} = \\frac{E_{\\text{KGE}_\\text{NP}}}
+           {2 - E_{\\text{KGE}_\\text{NP}}}
+
+    """
+    kgenp_ = kgenp(simulations, evaluation)[0, :]
     kgenp_c2m_ = kgenp_ / (2 - kgenp_)
 
     return kgenp_c2m_
